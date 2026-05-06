20e62a6df5787b033dba72564521fe6dd25a2c00
KaueSiqueira <kaue54150@gmail.com>
2026-05-06 18:28:18 -0300

    att

--- src/components/Fale.jsx
+++ src/components/Fale.jsx
@@ -22,10 +22,7 @@ function Fale() {
             <p>Sinalizaai@gmail.com</p>
           </div>
           <div className={styles.mapa}>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.1227833051876!2d-46.69433512489202!3d-23.52808587882268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef8775663b04f%3A0x923835e9005f8309!2sSenac%20Lapa%20Tito!5e0!3m2!1spt-BR!2sbr!4v1778101152979!5m2!1spt-BR!2sbr"
-              width="600"
-            ></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.1227833051876!2d-46.69433512489202!3d-23.52808587882268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef8775663b04f%3A0x923835e9005f8309!2sSenac%20Lapa%20Tito!5e0!3m2!1spt-BR!2sbr!4v1778101152979!5m2!1spt-BR!2sbr"></iframe>
           </div>
         </div>
 
